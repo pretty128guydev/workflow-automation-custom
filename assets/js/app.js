1884d0158f1186bc03f90b1bbcaf5d683aacda31
Magic Dev <rc.magic320@gmail.com>
2025-01-15 09:45:31 -0800

    update style and fix icon bug

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+window.setInterval(function() {
+  showSlides(slideIndex += 1);
+}, 5000)
+
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
