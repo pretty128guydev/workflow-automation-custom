d3c3a337c06142f8a32b5e07749067ae7a67f05b
Magic Dev <rc.magic320@gmail.com>
2025-01-15 23:22:28 -0800

    auto flow in feedback

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,23 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
+
+
+
+
+let items = document.getElementsByClassName('card');
+let itemCount = 8;
+let currentIndex = 1;
+
+function moveCard() {
+  if (currentIndex > itemCount) currentIndex = 0;
+  for (let i = 0; i < items.length; i++) {
+    document.getElementsByClassName('card')[i].style.transition = 'transform linear .3s';
+    const demo = -400 * currentIndex;
+    if (currentIndex == 0) document.getElementsByClassName('card')[i].style.transition = 'none';
+    document.getElementsByClassName('card')[i].style.transform = `translateX(${demo}px)`;
+  }
+  currentIndex++;
+}
+
+setInterval(function() {moveCard()}, 3000);
